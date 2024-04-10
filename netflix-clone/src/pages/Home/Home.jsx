67f1ae9196a1e83d6e30c5a623cd78f34bccf40d
Abelmekonn <Abelmekonn9@gmail.com>
2024-04-10 18:53:51 +0300

    Update App.jsx to render Home component; add global CSS reset to App.css
    * App.jsx: import and render Home component
    * App.css: add global CSS reset
    * index.css: uncomment CSS rules for :root selector

---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+
+import Footer from '../../Components/Footer/Footer' 
+function Home() {
+    return (
+        <>
+        <Header/>
+        <Footer />
+        </>
+    )
+}
+
+export default Home

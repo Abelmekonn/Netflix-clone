d70bb326c848f6f3b884317349959a938060001f
Abelmekonn <Abelmekonn9@gmail.com>
2024-04-10 22:16:04 +0300

    Your commit message

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import "./header.css"
+import Logo from '../../assets/image/logo (2).png'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+const Header = () => {
+    return (
+        <div  className='header-outer-container'>
+            <div className='header-container'>
+                <div className='header-left'>
+                    <ul>
+                        <li><img src={Logo} alt="" width={'100'} height={'100'} /></li>
+                        <li>Netflix</li>
+                        <li>Home</li>
+                        <li>Tv Show</li>
+                        <li>Movies</li>
+                        <li>Latest</li>
+                        <li>MyLists</li>
+                        <li>Browser my language</li>
+                    </ul>
+                </div>
+                <div className='header-right'>
+                    <ul>
+                        <li><SearchIcon /></li>
+                        <li><NotificationsIcon /></li>
+                        <li><AccountBoxIcon /></li>
+                        <li><ArrowDropDownIcon /></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header

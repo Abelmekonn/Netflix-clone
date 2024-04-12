93fa5834fcd0e10d85869d9e64ac48f6bea1f9f6
Abelmekonn <123172904+Abelmekonn@users.noreply.github.com>
2024-04-12 20:11:16 +0300

    request error

--- netflix-clone/src/utils/request.jsx
+++ netflix-clone/src/utils/request.jsx
@@ -1,12 +1,12 @@
-const API_KEY = process.env.REACT_APP_Apikey;
+const API_KEY = '2d9057b326d604a1c08e350489df0eb4';
 const requests = {
-    fetchTrending: ` /trending/all / week ? api_key = ${API_KEY}&language=en- US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks 213`,
-    fetchTopRatedMovies: `/movie/top_rated ? api_key = ${API_KEY} & language=en - US`,
-    fetchActionMovies: `/discover/movie ? api_key = ${API_KEY}& with_genres 28`,
-    fetchComedyMovies: `/discover/movie ? api_key = ${API_KEY}& with_genres=35`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/movie?api_key=${API_KEY}&with_networks213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}& with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie ? api_key = ${API_KEY}& with_genres 10749`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
     fetchDocumentaries: `/discover/movie?api_key=$(API_KEY}&with_genres=99`,
     fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
 };
